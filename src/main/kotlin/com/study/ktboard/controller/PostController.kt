8636ab7af52fbd41486ebd833ce5f0c8fcadbde9
cherhy <ekxk1234@naver.com>
2023-09-23 01:05:40 +0900

    조회 로직 작성

--- src/main/kotlin/com/study/ktboard/controller/PostController.kt
+++ src/main/kotlin/com/study/ktboard/controller/PostController.kt
@@ -3,15 +3,9 @@ package com.study.ktboard.controller
 import com.study.ktboard.controller.dto.*
 import com.study.ktboard.service.PostService
 import org.springframework.data.domain.Page
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.web.PageableDefault
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class PostController(
@@ -20,7 +14,7 @@ class PostController(
 
     @PostMapping("/posts")
     fun createPost(
-        @RequestBody postCreateRequest: PostCreateRequest,
+            @RequestBody postCreateRequest: PostCreateRequest,
         ): Long {
             return postService.createPost( postCreateRequest.toPostCreateRequestDto() )
     }
@@ -42,19 +36,17 @@ class PostController(
     }
 
     @GetMapping("/posts")
-    fun getPost(): Page<PostDetailRequest> {
-            return Page.empty()
-//        (
-//                PostDetailResponse(1, "title", "content", "cherry", LocalDateTime.now()),
-//                PostDetailResponse(2, "제목", "내용", "정철희", LocalDateTime.now()),
-//            )
-    }
+    fun getPost(
+        @PageableDefault(page = 0, size = 10) pageRequest: PageRequest
+    ): Page<PostDetailResponse> =
+        postService.getPosts(pageRequest)
+            .map(PostDetailResponse::toPostDetailResponse)
 
     @GetMapping("/posts/{id}")
     fun getPosts(
             @PathVariable id: Long,
-        ): PostDetailRequest {
-            return PostDetailRequest(1, "title", "content", "정철희", LocalDateTime.now())
-    }
+        ): PostDetailResponse =
+        postService.getPost(id)
+            .let(PostDetailResponse::toPostDetailResponse)
 
 }
