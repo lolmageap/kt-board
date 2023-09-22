9e7369a65dded99e15df78fceb7ea820b186fdf6
lolmageap <ekxk1234@gmail.com>
2023-09-22 17:24:13 +0900

    설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,8 +34,8 @@ dependencies {
 
 	testImplementation("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
-	testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+	testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.7.2")
 	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 	testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
 }
