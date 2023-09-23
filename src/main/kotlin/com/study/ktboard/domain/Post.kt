7831ef734cc66d877c80a14a03576f45f4d6488a
cherhy <ekxk1234@naver.com>
2023-09-23 23:35:14 +0900

    연관관계 설정

--- src/main/kotlin/com/study/ktboard/domain/Post.kt
+++ src/main/kotlin/com/study/ktboard/domain/Post.kt
@@ -2,34 +2,42 @@ package com.study.ktboard.domain
 
 import com.study.ktboard.exception.PostNotUpdatableException
 import com.study.ktboard.service.dto.PostUpdateRequestDto
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 
 @Entity
 class Post(
     createdBy: String,
     title: String,
-    content: String,
+    content: String
 ) : BaseEntity(
     createdBy = createdBy
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0
+
     var title: String = title
         protected set
+
     var content: String = content
         protected set
 
+    @OneToMany(mappedBy = "post", orphanRemoval = true, cascade = [CascadeType.ALL])
+    var comments: MutableList<Comment> = mutableListOf()
+        protected set
+
     fun update(postUpdateRequestDto: PostUpdateRequestDto) {
-        if (postUpdateRequestDto.updatedBy != this.createdBy)
+        if (postUpdateRequestDto.updatedBy != this.createdBy) {
             throw PostNotUpdatableException()
+        }
 
         this.title = postUpdateRequestDto.title
         this.content = postUpdateRequestDto.content
         super.update(postUpdateRequestDto.updatedBy)
     }
 }
-
