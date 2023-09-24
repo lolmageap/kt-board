e31395e513c49d42569a5e99d98d4746f851a3da
cherhy <ekxk1234@naver.com>
2023-09-24 21:52:35 +0900

    Tag 필드 추가

--- src/main/kotlin/com/study/ktboard/controller/dto/PostUpdateRequest.kt
+++ src/main/kotlin/com/study/ktboard/controller/dto/PostUpdateRequest.kt
@@ -5,12 +5,14 @@ import com.study.ktboard.service.dto.PostUpdateRequestDto
 data class PostUpdateRequest(
     val title: String,
     val content: String,
-    val updatedBy: String
+    val updatedBy: String,
+    val tags: List<String> = emptyList(),
 )
 
 fun PostUpdateRequest.toPostUpdateRequestDto(): PostUpdateRequestDto =
     PostUpdateRequestDto(
         title = this.title,
         updatedBy = this.updatedBy,
-        content = this.content
+        content = this.content,
+        tags = this.tags,
     )
