669b5ee1ab6f857c98d4a3994d77c53f6f7b8510
cherhy <ekxk1234@naver.com>
2023-09-25 23:19:40 +0900

    태그 변경 로직 작성

--- src/main/kotlin/com/study/ktboard/domain/Post.kt
+++ src/main/kotlin/com/study/ktboard/domain/Post.kt
@@ -45,6 +45,14 @@ class Post(
 
         this.title = postUpdateRequestDto.title
         this.content = postUpdateRequestDto.content
+        replaceTags(postUpdateRequestDto.tags)
         super.update(postUpdateRequestDto.updatedBy)
+    }
+
+    private fun replaceTags(tags: List<String>) {
+        if (this.tags.map { it.name } != tags) {
+            this.tags.clear()
+            this.tags.addAll(tags.map { Tag(it, this, this.createdBy) })
+        }
     }
 }
