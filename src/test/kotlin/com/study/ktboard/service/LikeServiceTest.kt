e6e30785dc2ddce3ae4a51ac7e6704d6c01f35aa
cherhy <ekxk1234@naver.com>
2023-09-27 20:10:13 +0900

    테스트 파일 생성

---
+++ src/test/kotlin/com/study/ktboard/service/LikeServiceTest.kt
@@ -0,0 +1,19 @@
+package com.study.ktboard.service
+
+import io.kotest.core.spec.style.BehaviorSpec
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LikeServiceTest(
+
+) : BehaviorSpec({
+
+    given("") {
+        When("") {
+
+            then(".") {
+            }
+        }
+    }
+
+})
