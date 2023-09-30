ddf02a8c87d216680334208685cab0705f21124c
cherhy <ekxk1234@naver.com>
2023-09-30 23:25:48 +0900

    레디스를 이용한 좋아요 처리

--- src/main/kotlin/com/study/ktboard/service/LikeService.kt
+++ src/main/kotlin/com/study/ktboard/service/LikeService.kt
@@ -4,6 +4,7 @@ import com.study.ktboard.domain.Like
 import com.study.ktboard.exception.PostNotFoundException
 import com.study.ktboard.repository.LikeRepository
 import com.study.ktboard.repository.PostRepository
+import com.study.ktboard.util.RedisUtil
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -13,14 +14,21 @@ import org.springframework.transaction.annotation.Transactional
 class LikeService(
     private val likeRepository: LikeRepository,
     private val postRepository: PostRepository,
+    private val redisUtil: RedisUtil,
 ) {
 
     fun createLike(postId: Long, createdBy: String): Long {
         val post = postRepository.findByIdOrNull(postId) ?: throw PostNotFoundException()
+        redisUtil.increment( redisUtil.getLikeCountKey(postId) )
         return likeRepository.save( Like(post, createdBy) ).id
     }
 
-    fun countLike(postId: Long): Long =
-        likeRepository.countByPostId(postId)
+    fun countLike(postId: Long): Long {
+        redisUtil.getCount( redisUtil.getLikeCountKey(postId) )?.let { return it }
+        with(likeRepository.countByPostId(postId)) {
+            redisUtil.setData( redisUtil.getLikeCountKey(postId) , this)
+            return this
+        }
+    }
 
 }
